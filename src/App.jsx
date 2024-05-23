cf5393d81991ab351d12805a75d620c704c7f5ad
Johan Swan <jswannieman@gmail.com>
2024-05-23 13:30:12 +1200

    Update App JSX Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
-import "./App.css";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
-  return <div>Hallo</div>;
+  const notify = () => toast("This is a toast notification !");
+  return (
+    <div>
+      <button onClick={notify}>Notify !</button>
+      <ToastContainer />
+    </div>
+  );
 }
 
 export default App;
